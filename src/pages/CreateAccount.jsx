1e526c0a8c487a26d65af267519e11c3f261d980
sideeq12 <abdwaheed2018@gmail.com>
2024-06-12 09:29:11 +0100

    account pages

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -14,7 +14,7 @@ function CreateAccount() {
          <h3 className='text-white font-semibold text-5xl'>LINKAGE</h3></div>
       <div className='w-full xl:w-[65vw] top-60 xl:top-0 flex flex-col justify-center align-middle
        absolute xl:relative xl:ml-auto bg-white rounded-tl-xl 
-      xl:rounded-tr-none xl:rounded-bl-xl h-[50rem] xl:h-full pt-10 px-4 xl:px-14'>
+      xl:rounded-tr-none xl:rounded-bl-xl h-[40rem] xl:h-full pt-10 px-4 xl:px-14'>
 
      <CreateAcc />
      {/* <SignIn /> */}
