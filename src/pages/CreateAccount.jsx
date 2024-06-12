ae30983c780256c46e948ac119ee3546ded370c1
sideeq12 <abdwaheed2018@gmail.com>
2024-06-12 09:42:05 +0100

    account pages

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -10,7 +10,7 @@ function CreateAccount() {
     <div className=' bg-grad relative h-screen w-screen 
      flex flex-col xl:flex-row '>
       <div className='w-full xl:w-[34vw] text-center pt-10 xl:pt-20'> 
-         <img src='OAU_linkage.png' className='w-20 xl:w-[80%] mx-auto' />
+         <img src='OAU_linkage.png' className='w-40 xl:w-[80%] mx-auto' />
          <h3 className='text-white font-semibold text-2xl xl:text-5xl'>LINKAGE</h3></div>
       <div className='w-full xl:w-[65vw] top-60 xl:top-0 flex flex-col justify-center align-middle
        absolute xl:relative xl:ml-auto bg-white rounded-tl-xl 
