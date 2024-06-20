84c1c44d7f2a5350e83d9cb16f134b92eaced23d
sideeq12 <abdwaheed2018@gmail.com>
2024-06-20 10:29:38 +0100

    dashboard

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -13,10 +13,50 @@ const divsArray = Array.from({ length: 20 });
 
 const Users = () => {
     const [view, setView]=useState(false)
+    const ViewProfile =()=>{
+        return(
+            <div className='bg-[#F1F1F1] w-[100vw] overflow-hidden lg:w-full h-full px-10 pt-10 lg:pt-32 '>
+    <div className='w-fit mx-auto  p-20 bg-white rounded-lg drop-shadow-lg'>
+    <div className='flex gap-3 mx-auto w-fit flex-wrap'>
+        <div className='bg-white  p-8 rounded-lg  w-fit'>
+        <img src='iconstest.jpg' className='w-40 h-40 rounded-lg'/></div>
+       <div>
+        <h1 className='text-2xl mb-4 font-bold'>Abiola Motunrayo</h1>
+        <div className='flex flex-wrap gap-4 w-96 h-fit text-lg'>
+            <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
+                <input name='upload'  id='upload' type='checkbox' className='h-10 rounded-md w-10'  />
+                <label htmlFor='upload' className='mt-2'>UPLOAD</label>
+            </div>
+            <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
+                <input name='delete'  id='delete' type='checkbox' className='h-10 rounded-md w-10'  />
+                <label htmlFor='delete' className='mt-2'>DELETE</label>
+            </div>
+            <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
+                <input name='edit'  id='edit' type='checkbox' className='h-10 rounded-md w-10'  />
+                <label htmlFor='edit' className='mt-2'>EDIT</label>
+            </div>
+            <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
+                <input name='download'  id='download' type='checkbox' className='h-10 rounded-md w-10'  />
+                <label htmlFor='download' className='mt-2'>DOWNLOAD</label>
+            </div>
+            </div>
+       </div>
+        </div>
+        <div onClick={()=>{setView(false)}} className=' hover:cursor-pointer ml-3 lg:mx-auto mt-3 w-60 lg:w-full bg-blue-800 text-center
+         text-white py-2 h-fit'>
+                Approve.
+            </div>
+    </div>
+        
+    
+    </div>
+    
+        )
+    }
   return (
     <div className='h-[100vh] w-[100vw] relative lg:flex'>
       <div className='w-[20vw] hidden lg:block pt-20 pl-10 border-r-2 h-full bg-gradient-to-t from-[#FECC48] to-[#010080]'>
-<img src="iconstest.svg"  className='h-2\16 mb-16' />
+<img src="iconstest.jpg"  className='h-20  w-20 mb-16 rounded-full' />
     <div className='mt-4'>
         <ul className='flex flex-col space-y-3 text-sm'>
             <li>
@@ -93,10 +133,10 @@ const Users = () => {
             </li>
         </ul>
       </div>
-   { view ? <ViewProfile /> :   <div className='lg:pl-14 lg:pt-3 bg-[#F1F1F1]  h-[92vh] w-full lg:w-[80vw] overflow-hidden'>
-      <div className='flex gap-2 lg:justify-between px-4 py-4 lg:p-0  lg:pr-20 bg-gradient-to-r from-[#FECC48]
+   { view ? <ViewProfile /> :   <div className='lg:pl-14 lg:py-5 bg-[#F1F1F1]  h-[92vh] w-full lg:w-[80vw] overflow-hidden'>
+      <div className='flex gap-2 lg:justify-between px-4 py-4   lg:pr-20 bg-gradient-to-r from-[#FECC48]
        to-[#010080] lg:bg-none'>
-        <img src="iconstest.svg" className='w-24 lg:hidden'/>
+        <img src="iconstest.jpg" className='w-24 lg:hidden'/>
         <h3 className='lg:text-2xl font-bold mt-4 text-base text-white lg:mt-0 lg:text-[#211A79]'>Welcome Admin <br/> Biola James</h3>
         <img src='/oaulogo.svg' className='w-12 ml-auto lg:hidden' />
         </div>
@@ -106,7 +146,7 @@ const Users = () => {
     {divsArray.map((_, index) => (
         <div key={index} onClick={()=>{setView(true)}} className="text-[#010080]  hover:cursor-pointer
          hover:bg-[#010080] hover:text-white p-2 lg:p-10  bg-white rounded-xl">
-            <img src="iconstest.svg"  className='auto h-10 lg:h-20 mb-2 mx-auto'/>
+            <img src="iconstest.jpg"  className='auto h-10 rounded-full  lg:h-20 mb-2 mx-auto'/>
             <span className='font-semibold'>Adewale James</span>
         </div>
       ))}
@@ -119,44 +159,5 @@ const Users = () => {
 
 export default Users
 
-const ViewProfile =()=>{
-    return(
-        <div className='bg-[#F1F1F1] w-[100vw] overflow-hidden lg:w-full h-full px-10 pt-10 lg:pt-32 '>
-<div className='w-fit mx-auto'>
-<div className='flex gap-3 mx-auto w-fit flex-wrap'>
-    <div className='bg-white  p-8 rounded-lg  w-fit'>
-    <img src='iconstest.svg' className='w-40 h-40'/></div>
-   <div>
-    <h1 className='text-2xl mb-4 font-bold'>Abiola Motunrayo</h1>
-    <div className='flex flex-wrap gap-4 w-96 h-fit text-lg'>
-        <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
-            <input name='upload'  id='upload' type='checkbox' className='h-10 rounded-md w-10'  />
-            <label htmlFor='upload' className='mt-2'>UPLOAD</label>
-        </div>
-        <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
-            <input name='delete'  id='delete' type='checkbox' className='h-10 rounded-md w-10'  />
-            <label htmlFor='delete' className='mt-2'>DELETE</label>
-        </div>
-        <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
-            <input name='edit'  id='edit' type='checkbox' className='h-10 rounded-md w-10'  />
-            <label htmlFor='edit' className='mt-2'>EDIT</label>
-        </div>
-        <div className='flex gap-3 bg-white p-4 rounded-md hover:cursor-pointer font-semibold'>
-            <input name='download'  id='download' type='checkbox' className='h-10 rounded-md w-10'  />
-            <label htmlFor='download' className='mt-2'>Download</label>
-        </div>
-        </div>
-   </div>
-    </div>
-    <div className=' hover:cursor-pointer ml-3 lg:mx-auto mt-3 w-60 lg:w-full bg-blue-800 text-center
-     text-white py-2 h-fit'>
-            Approve.
-        </div>
-</div>
-    
 
-</div>
-
-    )
-}
 
